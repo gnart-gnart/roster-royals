6c36744876e2ef2a73f204c730c5cf2b8a569296
judtfang0108 <judyfang0108@gmail.com>
2025-02-24 22:31:39 -0600

    edit nginx.conf

--- backend/roster_royals/settings.py
+++ backend/roster_royals/settings.py
@@ -106,9 +106,7 @@ MIDDLEWARE = [
 # During development only
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+
 CORS_ALLOW_HEADERS = [
     'accept',
     'accept-encoding',
