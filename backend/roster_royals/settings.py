bc3d065c23616a64573262774e9c5c6e6256908b
Grant <grantwoxford@gmail.com>
2025-03-16 22:19:38 -0500

    disabled append slash in settings.py

--- backend/roster_royals/settings.py
+++ backend/roster_royals/settings.py
@@ -47,14 +47,7 @@ TEMPLATES = [
     },
 ]
 
-# Database configuration (using default SQLite)
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
+APPEND_SLASH = False
 
 DATABASES = {
     'default': {
