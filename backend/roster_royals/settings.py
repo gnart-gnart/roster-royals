2535d97db0da125f87a86d3b5f2e812b14c36c03
Grant <grantwoxford@gmail.com>
2025-03-16 23:16:01 -0500

    updated settings

--- backend/roster_royals/settings.py
+++ backend/roster_royals/settings.py
@@ -141,17 +141,3 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
-
-# Add this middleware if not already present
-MIDDLEWARE = [
-    # Django default middleware
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  # Make sure this is before CommonMiddleware
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
-
