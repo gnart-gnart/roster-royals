2a7d3e471e7ffccb79591a9e86bbd4f59877ccb3
Grant <grantwoxford@gmail.com>
2025-04-06 16:05:48 -0500

    new migrations

--- backend/groups/migrations/0009_fix_sports_field.py
+++ backend/groups/migrations/0009_fix_sports_field.py
@@ -1,11 +1,17 @@
 from django.db import migrations, models
 
 def convert_sports_to_json(apps, schema_editor):
+    # Get the model using the apps registry to get the historical version
     League = apps.get_model('groups', 'League')
-    for league in League.objects.all():
-        if isinstance(league.sports, list):
-            league.sports = league.sports
-            league.save()
+    # Use a direct SQL query that doesn't require the image field
+    from django.db import connection
+    with connection.cursor() as cursor:
+        cursor.execute('SELECT id, sports FROM groups_league')
+        for row in cursor.fetchall():
+            league_id, sports = row
+            if isinstance(sports, list):
+                # No need to update since it's already a list
+                pass
 
 class Migration(migrations.Migration):
 
@@ -20,4 +26,4 @@ class Migration(migrations.Migration):
             name='sports',
             field=models.JSONField(blank=True, default=list),
         ),
-    ] 
+    ]
